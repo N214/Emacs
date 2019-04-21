8f164b8ead71997215bb025509bee41e94a7592c
Nicolas Tang <nico9156@gmail.com>
2019-04-21 23:53:58 +0200

    My commit message

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -93,7 +93,18 @@
 ;;----------------------------------------------------------------------------------
 (require 'helm)
 (require 'helm-config)
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 30)
+(helm-autoresize-mode 1)
 (helm-mode 1)
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
 
 ;;; Make DEL/BACKSPACE delete one character or the last path (if before a '/')
 ;; from https://github.com/hatschipuh/better-helm
