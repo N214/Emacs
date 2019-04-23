3c8e268fbb7136f65f10d7b36d11454c737d9487
Nicolas Tang <nico9156@gmail.com>
2019-04-23 12:49:01 +0200

    My commit message

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -14,6 +14,10 @@
 ;; C-c C-s Set schedule
 (global-set-key (kbd "C-c a") 'org-agenda)
 
+;; Enable transient mark mode
+(transient-mark-mode 1)
+(setq org-list-description-max-indent 5)
+
 
 ;; r aka remember
 (global-set-key (kbd "C-c r") 'org-capture)
@@ -23,6 +27,9 @@
  '(company-minimum-prefix-length 1)
 
  )
+
+;; org calendar
+
 
 
 (provide 'init-org)
