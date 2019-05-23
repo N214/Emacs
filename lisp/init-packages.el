5fb993ea803b119cb488e1d528372f84da4be0ca
Nicolas Tang <nico9156@gmail.com>
2019-05-24 01:43:42 +0200

    My commit message

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -322,4 +322,29 @@
    '(shell-pop-term-shell "/usr/bin/zsh")
    '(shell-pop-window-position "right")))
 
+;;------------------------Auctex
+
+(use-package tex
+  :defer t
+  :ensure auctex
+  :config
+  (setq TeX-auto-save t)
+  (add-hook 'LaTeX-mode-hook
+            (lambda ()
+              (rainbow-delimiters-mode)
+              (company-mode)
+              (smartparens-mode)
+              (turn-on-reftex)
+              (setq reftex-plug-into-AUCTeX t)
+              (reftex-isearch-minor-mode)
+              (setq TeX-PDF-mode t)
+              (setq TeX-source-correlate-method 'synctex)
+              (setq TeX-source-correlate-start-server t)))
+  (add-hook 'TeX-after-TeX-LaTeX-command-finished-hook
+	    #'TeX-revert-document-buffer)
+  (setq TeX-view-program-selection '((output-pdf "pdf-tools"))
+	TeX-source-correlate-start-server t)
+  (setq TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view"))))
+
+
 (provide 'init-packages)
